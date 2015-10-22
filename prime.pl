2f9fad6e11d85d54097b82d4e4185804d5d97bc9
Pedro <pedro.uriarte@globant.com>
2015-10-22 16:57:52 -0300

    changed

--- prime.pl
+++ prime.pl
@@ -9,14 +9,14 @@ my $start = time();
 if ($number >= 2 and $number <= 1000000 && $number =~ /\d+/){
 	my $chain.="1 ";
 	my $count;
-	for(my $i = 1; $i < $number; $i++){
+	for(my $i = 2; $i < $number; $i++){
 		$count = 0;
 		for(my $k = 1; $k < $number; $k++){
-			if($i eq $k || $i % $k eq 0){
+			if($i == $k || $i % $k == 0){
 				$count++;
 			}
 		}
-		if($count eq 2){
+		if($count == 2){
 			$chain.= $i." ";
 		}	
 	}
@@ -28,4 +28,4 @@ else{
 
 my $end = time();
 
-say "$end-$start seconds"; 
+say $end-$start ." seconds"; 
