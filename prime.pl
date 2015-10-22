3903486611ad7c7f04f0b2796ba930c3dcacf4fd
Pedro <pedro.uriarte@globant.com>
2015-10-22 15:51:34 -0300

    prime numbers

---
+++ prime.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+use v5.14;
+say "Insert integer number n from diapason 2..1000000:";
+my $number = <STDIN>;
+
+if ($number >= 2 and $number <= 1000000 && $number =~ /\d+/){
+	my $charm;
+	for(my $val = 1; $val <= $number; $val++){
+		if($val % 2 != 0 || $val == 2){
+			$charm.= $val." ";
+		}
+	}
+	print "$charm \n";
+}
+else{
+	die ("Bad input\n");
+}
+ 
