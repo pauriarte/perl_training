bc24e33dd07b9c659c1fb00be4877513462833ef
Pedro <pedro.uriarte@globant.com>
2015-10-22 16:46:36 -0300

    changed

--- prime.pl
+++ prime.pl
@@ -3,16 +3,29 @@ use v5.18;
 say "Insert integer number n from diapason 2..1000000:";
 my $number = <STDIN>;
 
+#say sqrt $number;
+
+my $start = time();
 if ($number >= 2 and $number <= 1000000 && $number =~ /\d+/){
-	my $charm;
-	for(my $val = 1; $val <= $number; $val++){
-		if($val % 2 != 0 || $val == 2){
-			$charm.= $val." ";
+	my $chain.="1 ";
+	my $count;
+	for(my $i = 1; $i < $number; $i++){
+		$count = 0;
+		for(my $k = 1; $k < $number; $k++){
+			if($i eq $k || $i % $k eq 0){
+				$count++;
+			}
 		}
+		if($count eq 2){
+			$chain.= $i." ";
+		}	
 	}
-	print "$charm \n";
+	print "$chain \n";
 }
 else{
 	die ("Bad input\n");
 }
- 
+
+my $end = time();
+
+say "$end-$start seconds"; 
