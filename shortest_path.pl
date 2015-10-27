398e149d7faa93b88bbf072e1192d1b7a6c9ebf0
Pedro <pedro.uriarte@globant.com>
2015-10-27 14:50:36 -0300

    end task 5

--- shortest_path.pl
+++ shortest_path.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 use v5.18;
 use Data::Dumper;
+use Paths::Graph;
 
 my $arg = <@ARGV>;
 open(NODES, "<:utf8", $arg) || die "Can't open numbers: $!\n";
@@ -8,25 +9,28 @@ binmode(STDOUT, ':utf8');
 
 my ($begin,$end) = split(" ",<NODES>); 
 
-my (%graph, $final, $path_size);
+my %graph;
 while(my $line = <NODES>){
-	my ($name,$neighbors) = split(" ",$line);
-	if(!exists $graph{$name}){
-		$graph{$name} = [];
-	}	
-	push @graph{$name}, $neighbors;
+	my ($name,$neighbor) = split(" ",$line);
+	$graph{$name}{$neighbor}=1;
 }
 
-for my $a (keys %graph){
-	if($a =~ /$begin/){
-		my $nodes = \@graph{$a};
+my $gr = Paths::Graph->new(-origin=>$begin,-destiny=>$end,-graph=>\%graph);
 
-		say $graph{$a}[0];
-		say @{$nodes};
-	}
+my @paths = $gr->shortest_path();
+
+my $cost;
+for my $path (@paths) {
+	$cost = $gr->get_path_cost(@$path);
+}
+
+if (not $cost){
+	die"-1\n";
+}
+else
+{
+	say $cost;
 }
 
 
-
-print Dumper \%graph;
 
