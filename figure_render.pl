fde680538cb9ae251edc8b0639990cfada3c38cd
Pedro <pedro.uriarte@globant.com>
2015-10-28 16:29:50 -0300

    task 6.

--- figure_render.pl
+++ figure_render.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 use v5.18;
 use Data::Dumper;
-
+use GD::Simple;
 #-------------------Class Coordinate--------------------------
 package Coordinate;
 
@@ -38,15 +38,16 @@ sub set_y {
 package Figure;
 use Data::Dumper;
 
+
 sub new {
 	my $class = shift;
-	my ($color, $coord) = @_;
+	my ($coord) = @_;
 
 	#print	Dumper \$coord;
 
 	my $this = bless {}, $class;
 
-	$this->{'color'} = $color;
+	$this->{'color'} = undef;
 	$this->{'coord'} = $coord;
 	return $this;
 }
@@ -68,6 +69,8 @@ sub set_color{
 #-------------------Class Rectangle--------------------------
 package Rectangle;
 use Data::Dumper;
+use GD::Simple;
+
 
 our @ISA = qw( Figure );
 
@@ -78,26 +81,38 @@ sub new {
 	#my $this = $class->SUPER::new($color, $coord);
 
 	my $this=shift; #Cogemos la clase que somos o una referencia a la clase (si soy un objeto)
-	my ($color, $coord) = @_;
+	my ($coord) = @_;
 	#print Dumper \$coord;
 	my $class = ref($this) || $this; #Averiguo la clase a la que pertenezco
-	my $self=$class->SUPER::new($color,$coord); #Inicializamos las propiedades con las usadas en Figure
+	my $self=$class->SUPER::new($coord); #Inicializamos las propiedades con las usadas en Figure
+    $self->{'color'} = "red";
+
 	bless $self, $class; #Se crea la clase
 	return ($self); #Devolvemos la clase recién construida
 }
 
 sub calculateArea(){
 	my @coord = @{shift->{'coord'}};
-	my ($x1,$x2,$x3,$x4,$y1,$y2,$y3,$y4) = ($coord[0]->get_x,$coord[1]->get_x,$coord[2]->get_x,$coord[3]->get_x,$coord[0]->get_y,$coord[1]->get_y,$coord[2]->get_y,$coord[3]->get_y);
+	my ($x1,$x2,$x3,$y1,$y2,$y3) = ($coord[0]->get_x,$coord[1]->get_x,$coord[2]->get_x,$coord[0]->get_y,$coord[1]->get_y,$coord[2]->get_y);
 	my $dist1 = sqrt (($x2-$x1)**2 + ($y2-$y1)**2);
-	my $dist2 = sqrt (($x4-$x3)**2 + ($y4-$y3)**2);
-	say $dist1;
-	say $dist2;
+	my $dist2 = sqrt (($x3-$x2)**2 + ($y3-$y2)**2);
+	#say $dist1;
+	#say $dist2;
 	return $dist1 * $dist2;
 }
 
-sub validate_coord(){
+sub draw(){
+    my $img = GD::Simple->new(200, 300);
+    my $this = shift;   
+    $img->bgcolor($this->{'color'});
+    $img->fgcolor($this->{'color'});
+	my @coord = @{$this->{'coord'}};
+	my ($x1,$x2,$y1,$y2) = ($coord[0]->get_x,$coord[2]->get_x,$coord[0]->get_y,$coord[2]->get_y);
 
+    $img->rectangle($x1,$y1,$x2,$y2);
+    open my $out, '>', 'rectangle_img.png' or die;
+    binmode $out;
+    print $out $img->png;
 }
 
 
@@ -111,10 +126,11 @@ our @ISA = qw( Rectangle );
 
 sub new {
 	my $this=shift; #Cogemos la clase que somos o una referencia a la clase (si soy un objeto)
-	my ($color, $coord) = @_;
+	my ($coord) = @_;
 	#print Dumper \$coord;
 	my $class = ref($this) || $this; #Averiguo la clase a la que pertenezco
-	my $self=$class->SUPER::new($color,$coord); #Inicializamos las propiedades con las usadas en Rectangle
+	my $self=$class->SUPER::new($coord); #Inicializamos las propiedades con las usadas en Rectangle
+    $self->{'color'} = "blue";
 	bless $self, $class; #Se crea la clase
 	return ($self); #Devolvemos la clase recién construida
 }
@@ -123,10 +139,23 @@ sub calculateArea(){
 	my @coord = @{shift->{'coord'}};
 	my ($x1,$x2,$y1,$y2) = ($coord[0]->get_x,$coord[1]->get_x,$coord[0]->get_y,$coord[1]->get_y);
 	my $dist = sqrt (($x2-$x1)**2 + ($y2-$y1)**2);
-	say $dist;
+	#say $dist;
 	return $dist**2;
 }
 
+sub draw(){
+    my $img = GD::Simple->new(200, 300);
+    my $this = shift;   
+    $img->bgcolor($this->{'color'});
+    $img->fgcolor($this->{'color'});
+	my @coord = @{$this->{'coord'}};
+	my ($x1,$x2,$y1,$y2) = ($coord[0]->get_x,$coord[2]->get_x,$coord[0]->get_y,$coord[2]->get_y);
+
+    $img->rectangle($x1,$y1,$x2,$y2);
+    open my $out, '>', 'square_img.png' or die;
+    binmode $out;
+    print $out $img->png;
+}
 
 1;
 
@@ -140,10 +169,11 @@ our @ISA = qw( Figure );
 
 sub new {
 	my $this=shift; #Cogemos la clase que somos o una referencia a la clase (si soy un objeto)
-	my ($color, $coord) = @_;
+	my ($coord) = @_;
 	#print Dumper \$coord;
 	my $class = ref($this) || $this; #Averiguo la clase a la que pertenezco
-	my $self=$class->SUPER::new($color,$coord); #Inicializamos las propiedades con las usadas en Figure
+	my $self=$class->SUPER::new($coord); #Inicializamos las propiedades con las usadas en Figure
+    $self->{'color'} = "green";
 	bless $self, $class; #Se crea la clase
 	return ($self); #Devolvemos la clase recién construida
 }
@@ -152,10 +182,24 @@ sub calculateArea(){
 	my @coord = @{shift->{'coord'}};
 	my ($x1,$x2,$y1,$y2) = ($coord[0]->get_x,$coord[1]->get_x,$coord[0]->get_y,$coord[1]->get_y);
 	my $dist = sqrt (($x2-$x1)**2 + ($y2-$y1)**2);
-	say "Pi: ".pi;
+	#say "Pi: ".pi;
 	return pi*$dist**2;
 }
 
+sub draw(){
+    my $img = GD::Simple->new(200, 300);
+    my $this = shift;   
+    #$img->bgcolor($this->{'color'});
+    #$img->fgcolor($this->{'color'});
+    #$img->moveTo(100,50);
+	my @coord = @{$this->{'coord'}};
+	my ($x1,$x2,$y1,$y2) = ($coord[0]->get_x,$coord[1]->get_x,$coord[0]->get_y,$coord[1]->get_y);
+   	my $dist = sqrt (($x2-$x1)**2 + ($y2-$y1)**2); 
+    $img->filledArc($x1,$y1,$dist,$dist,0,360,$img->colorAllocate(46,139,87));
+    open my $out, '>', 'circle_img.png' or die;
+    binmode $out;
+    print $out $img->png;
+}
 
 1;
 
@@ -168,10 +212,11 @@ our @ISA = qw( Figure );
 
 sub new {
 	my $this=shift; #Cogemos la clase que somos o una referencia a la clase (si soy un objeto)
-	my ($color, $coord) = @_;
+	my ($coord) = @_;
 	#print Dumper \$coord;
 	my $class = ref($this) || $this; #Averiguo la clase a la que pertenezco
-	my $self=$class->SUPER::new($color,$coord); #Inicializamos las propiedades con las usadas en Figure
+	my $self=$class->SUPER::new($coord); #Inicializamos las propiedades con las usadas en Figure
+    $self->{'color'} = "yellow";
 	bless $self, $class; #Se crea la clase
 	return ($self); #Devolvemos la clase recién construida
 }
@@ -180,61 +225,181 @@ sub calculateArea(){
 	my @coord = @{shift->{'coord'}};
 	my ($x1,$x2,$y1,$y2) = ($coord[0]->get_x,$coord[1]->get_x,$coord[0]->get_y,$coord[1]->get_y);
 	my $dist = sqrt (($x2-$x1)**2 + ($y2-$y1)**2);
-	say $dist;
+	#say $dist;
 	return (sqrt(3)/4)*$dist**2;
+}
+
+sub draw(){
+    my $img = GD::Simple->new(200, 300);
+    my $poly = GD::Polygon->new();
+
+    my $this = shift;   
+
+    
+    $img->bgcolor($this->{'color'});
+    $img->fgcolor($this->{'color'});
+	
+    my @coord = @{$this->{'coord'}};
+    my ($x1,$x2,$y1,$y2) = ($coord[0]->get_x,$coord[1]->get_x,$coord[0]->get_y,$coord[1]->get_y);
+    
+    my $x3 = $x1 + $x2 - (sqrt(3) * ($y1 - $y2));
+    my $y3 = $y1 + $y2 + (sqrt(3) * ($x1 - $x2));
+
+    $poly->addPt($x1,$y1);
+    $poly->addPt($x2,$y2);
+    $poly->addPt($x3,$y3);
+    $img->polygon($poly);
+    open my $out, '>', 'triangle_img.png' or die;
+    binmode $out;
+    print $out $img->png;
+}
+
+
+
+
+1;
+#-------------------Class Controller--------------------------
+
+package Controller;
+use Data::Dumper;
+
+sub new {
+	my $class = shift;
+    
+	my $this = bless {}, $class;
+
+	return $this;
+}
+
+sub createFigure {
+    my $this = shift;
+ 	$this->{'figure'} = shift;
+	$this->{'coord'} = shift;
+        
+    my $figure;
+    my $name = $this->{'figure'};    
+    my $coord = $this->{'coord'};
+    if($name =~ /Rectangle|Square|Circle|Triangle/){
+        $figure = $name->new($coord);
+    }
+    else
+    {
+        default {die "No figure named $name."};
+    }
+
+   	return $figure;
 }
 
 
 1;
-
 
 #-------------------Class main--------------------------
 package main;
 
 use Data::Dumper qw(Dumper);
 
-my $c1 = Coordinate->new(1,3);
-my $c2 = Coordinate->new(1,8);
-my $c3 = Coordinate->new(6,3);
-my $c4 = Coordinate->new(6,8);
+my $line;
+
+my $controller = Controller->new();
+
+my @figures;
+
+do{
+    $line = <STDIN>;
+    #say $line;
+    my @parameters=split(" ",$line);
+   # print Dumper \@parameters;
+    my $command = shift @parameters;
+    my $name = shift @parameters;
+    
+    ##say "Name: $name";
+     my ($x,$y);
+     my @array_coord;
+     for my $point (@parameters){
+        ($x,$y)=split(",",$point);
+        #say "x: $x y:$y";
+        my $coord = Coordinate->new($x,$y);
+        push @array_coord, $coord;
+    }
+   # print Dumper \@array_coord;
+    if($command =~/create/){
+        my $figure = $controller->createFigure($name,\@array_coord);
+        push @figures,$figure;
+    }
+   
+
+} until !defined($line) || $line eq "\n";
+
+for my $fig (@figures){
+    say $fig->get_color;
+    say $fig->calculateArea;
+    $fig->draw;
+}
+
+
+#my $c1 = Coordinate->new(1,3);
+#my $c2 = Coordinate->new(1,8);
+#my $c3 = Coordinate->new(6,3);
+#my $c4 = Coordinate->new(6,8);
 
 
 
-my $coordinates = [$c1, $c2, $c3, $c4];
+#my $coordinates = [$c1, $c2, $c3, $c4];
+
+#my $controller = Controller->new();
+
+#my $figure = $controller->createFigure("Rectangle",$coordinates);
+
+
+#my $area = $figure->calculateArea();
+#say $figure->get_color;
+#say $area;
+
+#$figure = $controller->createFigure("Square",$coordinates);
+
+
+#$area = $figure->calculateArea();
+#say $figure->get_color;
+#say $area;
+
+#$figure = $controller->createFigure("Circle",$coordinates);
+
+
+#$area = $figure->calculateArea();
+#say $figure->get_color;
+#say $area;
+
+#$figure = $controller->createFigure("Triangle",$coordinates);
+
+
+#$area = $figure->calculateArea();
+#say $figure->get_color;
+#say $area;
+
+
 #print Dumper \@coordinates;
 
 #my $f = Figure->new('blue', $coordinates);
-my $r = Rectangle->new('blue', $coordinates);
-my $s = Square->new('red', $coordinates);
-my $c = Circle->new('yellow', $coordinates);
-my $t = Triangle->new('green', $coordinates);
-#$c1->set_x(333);
-#say $c1->get_x;
-#say $c1->get_y;
-#$f->set_color("red");
-#say $f->get_color;
-#my @cor = $f->get_coord;
-#print Dumper \$cor[1]->get_x;
-#print Dumper \$cor[1]->get_y;
-my $area = $r->calculateArea();
-$r->set_color("orange");
-say $r->get_color;
-say $area;
+#my $r = Rectangle->new('blue', $coordinates);
+#my $s = Square->new('red', $coordinates);
+#my $c = Circle->new('yellow', $coordinates);
+#my $t = Triangle->new('green', $coordinates);
 
-$area = $s->calculateArea();
-say $s->get_color;
-say $area;
+#my $area = $r->calculateArea();
+#$r->set_color("orange");
+#say $r->get_color;
+#say $area;
 
-$area = $c->calculateArea();
-say $c->get_color;
-say $area;
+#$area = $s->calculateArea();
+#say $s->get_color;
+#say $area;
 
-$area = $t->calculateArea();
-say $t->get_color;
-say $area;
+#$area = $c->calculateArea();
+#say $c->get_color;
+#say $area;
 
-
-
-
+#$area = $t->calculateArea();
+#say $t->get_color;
+#say $area;
 
 1;
